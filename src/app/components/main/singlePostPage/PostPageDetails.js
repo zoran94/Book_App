670dd6097b1b7a60552a34eaacb8ce70b7d8c0c0
BIT Student <classroom@bgit.rs>
2019-04-11 16:47:45 +0200

    button disable option task completed

--- src/app/components/main/singlePostPage/PostPageDetails.js
+++ src/app/components/main/singlePostPage/PostPageDetails.js
@@ -4,83 +4,77 @@ import SinglePostItem from "./postDetails/SinglePostItem";
 import createComment from "../../../../services/createComment";
 
 class PostPageDetails extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            post: null,
-            comments: [],
-            commentValue: ""
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: null,
+      comments: [],
+      commentValue: ""
+    };
+    this.onCreateComment = this.onCreateComment.bind(this);
+    this.onChangeCommentValue = this.onChangeCommentValue.bind(this);
+  }
 
-        }
-        this.onCreateComment = this.onCreateComment.bind(this);
-        this.onChangeCommentValue = this.onChangeCommentValue.bind(this);
+  componentDidMount() {
+    this.onLoadPosts();
+    this.onLoadComments();
+  }
+
+  onLoadPosts() {
+    const postId = this.props.match.params.id;
+    data.fetchSinglePost(postId).then(post => {
+      this.setState({
+        post: post
+      });
+    });
+  }
+
+  onLoadComments() {
+    const postId = this.props.match.params.id;
+    data
+      .fetchComments(postId)
+      .then(comments => {
+        this.setState({
+          comments: comments
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  onChangeCommentValue(e) {
+    this.setState({ commentValue: e.target.value });
+  }
+
+  onCreateComment() {
+    const postId = this.props.match.params.id;
+    createComment(this.state.commentValue, postId).then(() => {
+      this.onLoadComments();
+      this.setState({
+        commentValue: ""
+      });
+    });
+  }
+
+  render() {
+    if (!this.state.post) {
+      return <h1>Loading ...</h1>;
     }
 
-
-
-    componentDidMount() {
-        this.onLoadPosts();
-        this.onLoadComments();
-    }
-
-
-    onLoadPosts() {
-        const postId = this.props.match.params.id;
-        data.fetchSinglePost(postId)
-            .then(post => {
-                this.setState({
-                    post: post
-                })
-            })
-    }
-
-    onLoadComments() {
-        const postId = this.props.match.params.id;
-        data.fetchComments(postId)
-            .then(comments => {
-                this.setState({
-                    comments: comments
-                })
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    }
-
-
-    onChangeCommentValue(e) {
-        this.setState({ commentValue: e.target.value });
-    }
-
-    onCreateComment() {
-        const postId = this.props.match.params.id;
-        createComment(this.state.commentValue, postId)
-            .then(() => {
-                this.onLoadComments();
-                this.setState({
-                    commentValue: ""
-                })
-            })
-    }
-
-
-    render() {
-        if ((!this.state.post)) {
-            return <h1>Loading ...</h1>
-        }
-
-        return (
-            <div className="padding-top">
-                <SinglePostItem
-                    onCreateComment={this.onCreateComment}
-                    onChangeCommentValue={this.onChangeCommentValue}
-                    commentValue={this.state.commentValue}
-                    post={this.state.post}
-                    comments={this.state.comments} />
-            </div>
-        )
-
-    }
+    return (
+      <div className="padding-top">
+        <SinglePostItem
+          onCreateComment={this.onCreateComment}
+          onChangeCommentValue={this.onChangeCommentValue}
+          commentValue={this.state.commentValue}
+          post={this.state.post}
+          comments={this.state.comments}
+          disabled={!this.state.commentValue.length}
+        />
+      </div>
+    );
+  }
 }
 
 export default PostPageDetails;
--- src/app/components/main/singlePostPage/postDetails/SinglePostItem.js
+++ src/app/components/main/singlePostPage/postDetails/SinglePostItem.js
@@ -4,74 +4,51 @@ import SingleImagePost from "./SingleImagePost";
 import SingleTextPost from "./SingleTextPost";
 import SingleComment from "./SingleComment";
 
-const SinglePostItem = (props) => {
-
-    const postItem = () => {
-        if (props.post.type === 'video') {
-            return <SingleVideoPost post={props.post} />
-        } else if (props.post.type === "image") {
-            return <SingleImagePost post={props.post} comments={[]} />
-        } else {
-            return <SingleTextPost post={props.post} comments={[]} />
-        }
+const SinglePostItem = props => {
+  const postItem = () => {
+    if (props.post.type === "video") {
+      return <SingleVideoPost post={props.post} />;
+    } else if (props.post.type === "image") {
+      return <SingleImagePost post={props.post} comments={[]} />;
+    } else {
+      return <SingleTextPost post={props.post} comments={[]} />;
     }
+  };
 
+  return (
+    <>
+      {postItem()}
+      <form>
+        <div className="row">
+          <div className="input-field col s12">
+            <textarea
+              onChange={props.onChangeCommentValue}
+              value={props.commentValue}
+              id="textarea2"
+              className="materialize-textarea"
+              data-length="120"
+            />
+            <label htmlFor="textarea2" />
+            <button
+              onClick={props.onCreateComment}
+              className="right btn"
+              disabled={props.disabled}
+            >
+              Post Comment
+            </button>
+          </div>
+        </div>
+      </form>
 
-
-    if (!props.comments.length) {
-        return (
-            <>
-                {postItem()}
-                <form>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <textarea
-                                onChange={props.onChangeCommentValue}
-                                value={props.commentValue}
-                                id="textarea2"
-                                className="materialize-textarea"
-                                data-length="120">
-                            </textarea>
-                            <label htmlFor="textarea2"></label>
-                            <button onClick={props.onCreateComment} className="right btn">Post Comment</button>
-                        </div>
-                    </div>
-                </form>
-                <h1 className="center">No comments</h1>
-            </>
-        )
-    }
-    return (
-        <>
-
-            {postItem()}
-            <form>
-                <div className="row">
-                    <div className="input-field col s12">
-
-                        <textarea
-                            onChange={props.onChangeCommentValue}
-                            value={props.commentValue}
-                            id="textarea2"
-                            className="materialize-textarea"
-                            data-length="120">
-                        </textarea>
-
-                        <label htmlFor="textarea2"></label>
-                        <button onClick={props.onCreateComment} className="right btn">Post Comment</button>
-                    </div>
-                </div>
-            </form>
-
-
-            {
-                props.comments.map(comment => {
-                    return <SingleComment comment={comment} key={comment.id} />
-                })
-            }
-        </>
-    )
-
-}
+      {!props.comments.length ? (
+        <h1 className="center">No comments</h1>
+      ) : (
+        props.comments.map(comment => {
+          return <SingleComment comment={comment} key={comment.id} />;
+        })
+      )}
+    </>
+  );
+};
 
 export default SinglePostItem;
