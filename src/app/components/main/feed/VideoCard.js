d5764c67eddcc18fd2755e883049b48cfdb7935d
BIT Student <classroom@bgit.rs>
2019-04-11 15:38:31 +0200

    feature 11 completed, refactoring done to some files

--- src/app/components/main/feed/VideoCard.js
+++ src/app/components/main/feed/VideoCard.js
@@ -11,12 +11,12 @@ const VideoCard = (props) => {
             <div className="row">
                 <div className="col s8 offset-s2 ">
                     <div className="card post-bg-color">
-                        <div className="card-content white-text center  rounded-corners">
-                            <iframe width="700" height="300" src={videoUrl} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        <div className="card-style white-text center">
+                            <iframe width="100%" height="300" src={videoUrl} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                         </div>
                         <div className="post-info">
-                            <span>Video post</span>
-                            <Link to={`/post/${id}`}><span className="right">{comments.length} Comments</span></Link>
+                            <span className=""><i className="fas fa-video"></i> Video post</span>
+                            <Link to={`/post/${id}`}><span className="right"><i className="far fa-comment"></i> {comments.length} Comments</span></Link>
                         </div>
                     </div>
                 </div>
